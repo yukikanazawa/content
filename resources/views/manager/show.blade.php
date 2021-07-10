ce3889fdd74a767fc13d863c27222b5d7bb42533
EC2 Default User <ec2-user@ip-172-31-19-48.ap-northeast-1.compute.internal>
2021-07-10 23:06:46 +0900

    コメント削除  Procfile for Heroku

--- resources/views/manager/show.blade.php
+++ resources/views/manager/show.blade.php
@@ -15,21 +15,32 @@
                             <h3><a href="/manager/initials/{{ $initial->id }}/{{ $overview->id }}/long">長文要約</a></h3>
                             <h3><a href="/manager/initials/{{ $initial->id }}/{{ $overview->id }}/edit">要約の編集</a></h3>
                             <h3><a class='delete'><span onclick="return deletePost();">要約の消去</span></a></h3>
-                            <a href='/manager' class='back'>[戻る]</a>
+                            <a href='/manager/initials/{{ $initial->id }}/' class='back'>[戻る]</a>
                     </form></br>
                 </div>
             </div>
             <div class="comment">
+                <h2>新規のコメントを作成</h2>
+                <form action="/initials/{{ $initial->id }}/{{ $overview->id }}/store" method="POST">
+                    {{ csrf_field() }}
+                    <div class="usercomment">
+                        ユーザーネーム</br>
+                        <input type="text" name="comment[name]" value="{{ Auth::user()->name }}" size="48" readonly /> </br> 
+                        この作品の感想</br>
+                        <textarea name="comment[body]" placeholder="感想を記載" rows="5" cols="65"></textarea>
+                        <input type="submit" value="保存"/>
+                    </div>
+                </form>
                 <h2>コメント欄</h2></br></br>
                 @foreach ($comments as $comment)
                 <div class="flex">
                 <p>・{{ $comment->name }}　</p>
-                <form method="POST" action="/initials/{{ $initial->id }}/delete" id="form_delete">
+                <form method="POST" action="/initials/{{ $initial->id }}/delete" id="form_destroy">
                     @csrf
                     @method('DELETE')
                         <input type="hidden" name="comment_id" value='{{ $comment->id }}'/>
                         <input type="submit" style="display:none">
-                        <button class='destroy'><span onclick="return deleteComment();">削除</span></button>
+                        <button onclick="return deleteComment();">削除</button>
                 </form>
                 </div>
                 <p>{{ $comment->body }}</p></br>
@@ -50,7 +61,7 @@
         function deleteComment(){
             'use strict';
             if (confirm('本当にコメントを削除しますか？')){
-                document.getElementById('form_delete').submit();
+                document.getElementById('form_destroy').submit();
             } else{
                 return false;    
             }
